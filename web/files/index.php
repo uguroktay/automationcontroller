301af3f1a3e90a1eaafb2d8f10387a12a46c0944
uguroktay <56156686+uguroktay@users.noreply.github.com>
2023-11-17 09:59:34 +0300

    Update index.php

--- web/files/index.php
+++ web/files/index.php
@@ -1,1 +1,20 @@
 
+
+<html>
+
+<head>
+
+<title>Ansible Automation Controller Essentials</title>
+
+</head>
+
+<body>
+
+<h1>Welcome to Ansible Automation Controller Essentials Course !</h1>
+<p> App backend <?=gethostname()?> </p>
+
+<img src="https://loremflickr.com/320/240" />
+
+</body>
+
+</html>
